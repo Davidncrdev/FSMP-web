264d7fbb2c181e51814bec0b9e968e0802a595f7
David Nacro <166960840+Davidncrdev@users.noreply.github.com>
2024-06-27 13:40:55 +0200

    cambios de contaco

--- src/pages/api/send-email.js
+++ src/pages/api/send-email.js
@@ -1,45 +1,39 @@
 import nodemailer from 'nodemailer';
-import 'dotenv/config';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export async function post({ request }) {
+  const formData = await request.formData();
+  const name = formData.get('name');
+  const email = formData.get('email');
+  const reason = formData.get('reason');
+  const message = formData.get('message');
+
+  if (!name || !email || !reason || !message) {
+    return new Response(JSON.stringify({ message: 'Todos los campos son requeridos.' }), { status: 400 });
+  }
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: 'Davidkabre25@gmail.com',
+    subject: `Nuevo mensaje de contacto de ${name}`,
+    text: `Nombre: ${name}\nCorreo: ${email}\nMotivo: ${reason}\nMensaje: ${message}`,
+  };
+
   try {
-    const formData = await request.formData();
-    const name = formData.get('name');
-    const email = formData.get('email');
-    const reason = formData.get('reason');
-    const message = formData.get('message');
-
-    const transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
-
-    const mailOptions = {
-      from: process.env.EMAIL_USER,
-      to: 'Davidkabre25@gmail.com',
-      subject: `Nuevo mensaje de ${name} - ${reason}`,
-      text: `
-        Nombre: ${name}
-        Correo: ${email}
-        Razón: ${reason}
-        Mensaje: ${message}
-      `,
-    };
-
-    const info = await transporter.sendMail(mailOptions);
-
-    console.log('Correo enviado: %s', info.messageId);
-
-    return new Response(JSON.stringify({ message: 'Correo enviado exitosamente' }), {
-      status: 200,
-    });
+    await transporter.sendMail(mailOptions);
+    return new Response(JSON.stringify({ message: 'Correo enviado exitosamente.' }), { status: 200 });
   } catch (error) {
     console.error('Error enviando el correo:', error);
-    return new Response(JSON.stringify({ message: 'Error enviando el correo', error: error.message }), {
-      status: 500,
-    });
+    return new Response(JSON.stringify({ message: 'Error enviando el correo.' }), { status: 500 });
   }
 }
